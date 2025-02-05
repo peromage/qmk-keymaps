1466b6e947dc08ea980d9b81e71230ad62f0ad12
Fang Deng <fang@elfang.com>
2025-02-05 18:39:14 -0500

    Update Planck EZ configuration

--- planck_ez_glow_prmg01-comfy/zsa_planck_ez_glow_prmg01-comfy_source/config.h
+++ planck_ez_glow_prmg01-comfy/zsa_planck_ez_glow_prmg01-comfy_source/config.h
@@ -13,7 +13,6 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 
-#define ORYX_CONFIGURATOR
 #define ONESHOT_TAP_TOGGLE 2
 
 #undef ONESHOT_TIMEOUT
@@ -23,10 +22,8 @@
 #define RGB_MATRIX_TIMEOUT 300000
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define FIRMWARE_VERSION u8"BPqo0/vPNKb"
-#define RAW_USAGE_PAGE 0xFF60
-#define RAW_USAGE_ID 0x61
-#define LAYER_STATE_8BIT
+#define SERIAL_NUMBER "BPqo0/rbK004"
+#define LAYER_STATE_16BIT
 #define COMBO_COUNT 4
 
 #define RGB_MATRIX_STARTUP_SPD 60
